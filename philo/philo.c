d57c62d7b19bacf8fdabf5e86054b013e2bd411f
Safar Safarly <hafl@me.com>
2021-10-21 02:22:12 +0300

    threads and mutex testing

--- philo/philo.c
+++ philo/philo.c
@@ -1,7 +1,42 @@
 #include "philo.h"
 
+#define PH_COUNT	100
+
+void	*test(void *arg)
+{
+	t_philo	*ph = arg;
+
+	for (int i = 0; i < 100000; i++) {
+		pthread_mutex_lock(&ph->mutex);
+		ph->sum++;
+		pthread_mutex_unlock(&ph->mutex);
+	}
+	return (NULL);
+}
+
 int	main(void)
 {
-	printf("hi from philo\n");
+	pthread_t	philos[PH_COUNT];
+	t_philo		ph;
+	int			i = 0;
+
+	pthread_mutex_init(&ph.mutex, NULL);
+	ph.sum = 0;
+	while (i < PH_COUNT)
+	{
+		if (pthread_create(&philos[i], NULL, &test, &ph))
+			return (1);
+		i++;
+	}
+	i = 0;
+	while (i < PH_COUNT)
+	{
+		if (pthread_join(philos[i], NULL))
+			return (1);
+		i++;
+	}
+	printf("hi from philo, sum is %d\n", ph.sum);
+	pthread_mutex_destroy(&ph.mutex);
+
 	return (0);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -2,5 +2,13 @@
 # define PHILO_H
 
 # include	<stdio.h>
+# include	<stdlib.h>
+# include	<pthread.h>
+
+typedef struct s_philo
+{
+	pthread_mutex_t		mutex;
+	int					sum;
+}						t_philo;
 
 #endif
