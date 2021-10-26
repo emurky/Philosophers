ff5d182911d049c33c634b4058e854b054e49f64
Safar Safarly <hafl@me.com>
2021-10-26 04:17:48 +0300

    working on threading, saving changes

--- philo/philo.c
+++ philo/philo.c
@@ -15,22 +15,21 @@ size_t	gettime_in_ms(void)
 	return (current_time_in_milliseconds);
 }
 
-// void	*test(void *arg)
-// {
-// 	t_philo	*ph = arg;
-// 	// int		count = 0;
+void	sleep_wrapper(size_t ms)
+{
+	size_t	current_time;
 
-// 	for (int i = 0; i < 100000; i++) {
-// 		// count++;
-// 		pthread_mutex_lock(&ph->mutex);
-// 		ph->sum++;
-// 		pthread_mutex_unlock(&ph->mutex);
-// 	}
-// 	// pthread_mutex_lock(&ph->mutex);
-// 	// ph->sum += count;
-// 	// pthread_mutex_unlock(&ph->mutex);
-// 	return (NULL);
-// }
+	current_time = gettime_in_ms();
+	while (gettime_in_ms() < current_time + ms)
+		usleep(50);
+}
+
+void	print_status(t_philo *philo, char *status)
+{
+	pthread_mutex_lock(&philo->all->print_mtx);
+	printf("%zu %zu %s\n", gettime_in_ms() - philo->all->start_time, philo->id, status);
+	pthread_mutex_unlock(&philo->all->print_mtx);
+}
 
 bool	parser(int argc, char **argv, t_all *args)
 {
@@ -59,35 +58,75 @@ bool	parser(int argc, char **argv, t_all *args)
 	return (true);
 }
 
-bool	start_threading(t_all *all)
+void	*philo_thread(void *arg)
+{
+	t_philo		*philo;
+
+	philo = (t_philo *)arg;
+	if (philo->id % 2 == ODD)
+		sleep_wrapper(philo->all->time_to_eat);
+	print_status(philo, STATUS_FORK);
+	while (true)
+	{
+
+	}
+	return (NULL);
+}
+
+bool	philos_init(t_all *all, t_philo **philos)
 {
 	unsigned int	i;
-	t_philo			*philos;
+	// t_philo			*philos;
 	pthread_mutex_t	*forks;
 
 	i = 0;
-	philos = malloc(sizeof(t_philo) * all->philo_count);
+	*philos = malloc(sizeof(t_philo) * all->philo_count);
 	forks = malloc(sizeof(pthread_mutex_t) * all->philo_count);
-	if (!philos || !forks)
+	if (!(*philos) || !forks)
 		return (print_error(ERR_MALLOC));
 	while (i < all->philo_count)
 	{
 		if (pthread_mutex_init(&forks[i], NULL))
 			return (print_error(ERR_MTX_INIT));
-		if (pthread_mutex_init(&philos[i].death_mtx, NULL))
+		if (pthread_mutex_init(&(*philos)[i].death_mtx, NULL))
 			return (print_error(ERR_MTX_INIT));
-		philos[i].left_fork_mtx = &forks[i];
-		philos[i].right_fork_mtx = &forks[(i + 1) % all->philo_count];
-		philos[i].id = i + 1;
-		philos[i].meals = all->meals;
+		(*philos)[i].left_fork_mtx = &forks[i];
+		(*philos)[i].right_fork_mtx = &forks[(i + 1) % all->philo_count];
+		(*philos)[i].id = i + 1;
+		(*philos)[i].meals = all->meals;
+		(*philos)[i].all = all;
+		(*philos)[i].dead = false;
+		// if (pthread_create(&(*philos)[i].thread, NULL, &philo_thread, &philos[i]))
+		// 	return(print_error(ERR_THRD_CRT));
+		// if (pthread_detach((*philos)[i].thread))
+		// 	return(print_error(ERR_THRD_DTC));
 		i++;
 	}
-	all->philos = philos;
+	// i = 0;
+	// while (i < all->philo_count)
+	// {
+	// 	if (pthread_mutex_init(&forks[i], NULL))
+	// 		return (print_error(ERR_MTX_INIT));
+	// 	if (pthread_mutex_init(&(*philos)[i].death_mtx, NULL))
+	// 		return (print_error(ERR_MTX_INIT));
+	// 	(*philos)[i].left_fork_mtx = &forks[i];
+	// 	(*philos)[i].right_fork_mtx = &forks[(i + 1) % all->philo_count];
+	// 	(*philos)[i].id = i + 1;
+	// 	(*philos)[i].meals = all->meals;
+	// 	(*philos)[i].all = all;
+	// 	// if (pthread_create(&(*philos)[i].thread, NULL, &philo_thread, &philos[i]))
+	// 	// 	return(print_error(ERR_THRD_CRT));
+	// 	// if (pthread_detach((*philos)[i].thread))
+	// 	// 	return(print_error(ERR_THRD_DTC));
+	// 	i++;
+	// }
+	// all->philos = philos;
 	all->forks = forks;
+
 	return (true);
 }
 
-bool	clean_philos(t_all *all)
+bool	clean_philos(t_all *all, t_philo *philos)
 {
 	unsigned int	i;
 
@@ -96,51 +135,65 @@ bool	clean_philos(t_all *all)
 		return (print_error(ERR_MTX_DSTR));
 	while (i < all->philo_count)
 	{
-		if (pthread_mutex_destroy(&all->philos[i].death_mtx))
+		if (pthread_mutex_destroy(&philos[i].death_mtx))
 			return (print_error(ERR_MTX_DSTR));
 		if (pthread_mutex_destroy(&all->forks[i]))
 			return (print_error(ERR_MTX_DSTR));
 		i++;
 	}
-	free(all->philos);
+	free(philos);
 	free(all->forks);
+	return (true);
+}
+
+bool	threads_init(t_philo *philos)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < philos->all->philo_count)
+	{
+		if (pthread_create(&philos[i].thread, NULL, &philo_thread, &philos[i]))
+			return(print_error(ERR_THRD_CRT));
+		if (pthread_detach(philos[i].thread))
+			return(print_error(ERR_THRD_DTC));
+		i++;
+	}
 	return (true);
 }
 
 int	main(int argc, char **argv)
 {
-	// pthread_t		philos[PH_COUNT];
-	// t_philo			ph;
-	// int				i = 0;
-
 	t_all			all;
+	t_philo			*philos;
 
-	if (!parser(argc, argv, &all) || !start_threading(&all))
-	{
+	if (!parser(argc, argv, &all) || !philos_init(&all, &philos))
 		return (1);
+
+	// for (int i = 0; i < 5; i++) {
+	// 	printf("%zu id from main\n", philos[i].id);
+	// }
+
+	threads_init(philos);
+
+// ssize_t i =0;
+// 	while (i < all.philo_count)
+// 	{
+// 		if (pthread_create(&philos[i].thread, NULL, &philo_thread, &philos[i]))
+// 			return(print_error(ERR_THRD_CRT));
+// 		if (pthread_detach(philos[i].thread))
+// 			return(print_error(ERR_THRD_DTC));
+// 		i++;
+// 	}
+	// printf("%d %d %d %d %d\n", all.philo_count, all.time_to_die, all.time_to_eat, all.time_to_sleep, all.meals);
+
+	sleep(5);
+
+	for (size_t i = 0; i < all.philo_count; i++) {
+		pthread_join(philos[i].thread, NULL);
 	}
-	
 
-	printf("%d %d %d %d %d\n", all.philo_count, all.time_to_die, all.time_to_eat, all.time_to_sleep, all.meals);
-	// pthread_mutex_init(&ph.mutex, NULL);
-	// ph.sum = 0;
-	// while (i < PH_COUNT)
-	// {
-	// 	if (pthread_create(&philos[i], NULL, &test, &ph))
-	// 		return (1);
-	// 	i++;
-	// }
-	// i = 0;
-	// while (i < PH_COUNT)
-	// {
-	// 	if (pthread_join(philos[i], NULL))
-	// 		return (1);
-	// 	i++;
-	// }
-	// printf("hi from philo, sum is %d\n", ph.sum);
-	// pthread_mutex_destroy(&ph.mutex);
-
-	if (!clean_philos(&all))
+	if (!clean_philos(&all, philos))
 		return (1);
 
 	return (0);
--- philo/philo.h
+++ philo/philo.h
@@ -11,24 +11,23 @@
 
 # define THREADS_LIMIT	420
 
+# define EVEN			0
+# define ODD			1
+
+# define STATUS_FORK	"has taken a fork"
+# define STATUS_EAT		"is eating"
+# define STATUS_SLEEP	"is sleeping"
+# define STATUS_THINK	"is thinking"
+# define STATUS_DEATH	"died"
+
 # define ERR_ARGS		"Wrong number of arguments. Must be 4 or 5\n"
 # define ERR_NUM		"One of arguments is not numerical. Arguments must be \
 positive integers\n"
 # define ERR_MALLOC		"Malloc error\n"
-# define ERR_MTX_INIT		"Mutex initialisation\n"
-# define ERR_MTX_DSTR		"Mutex destroying\n"
-
-typedef struct s_philo
-{
-	pthread_t			thread;
-	pthread_mutex_t		death_mtx;
-	pthread_mutex_t		*left_fork_mtx;
-	pthread_mutex_t		*right_fork_mtx;
-	size_t				meals;
-	size_t				id;
-
-	int					sum;
-}				t_philo;
+# define ERR_MTX_INIT	"Mutex initialisation\n"
+# define ERR_MTX_DSTR	"Mutex destroying\n"
+# define ERR_THRD_CRT	"Thread creating\n"
+# define ERR_THRD_DTC	"Thread detaching\n"
 
 typedef struct s_all
 {
@@ -40,9 +39,26 @@ typedef struct s_all
 	size_t				start_time;
 	pthread_mutex_t		print_mtx;
 
-	t_philo				*philos;
+	// t_philo				*philos;
 	pthread_mutex_t		*forks;
+
+	int					sum;
 }				t_all;
+
+typedef struct s_philo
+{
+	pthread_t			thread;
+	pthread_mutex_t		death_mtx;
+	pthread_mutex_t		*left_fork_mtx;
+	pthread_mutex_t		*right_fork_mtx;
+	size_t				meals;
+	size_t				id;
+
+	bool				dead;
+	t_all				*all;
+
+	int					sum;
+}				t_philo;
 
 size_t			ft_strlen(char *str);
 bool			print_error(char *err_str);
